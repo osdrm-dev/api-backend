50d6db30aacd1a726ca1ec74702e29ff16e1258b
Briano <briano.manitra@gmail.com>
2026-01-07 20:59:53 +0200

    OSDRM-10 Open API Documentation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TaskModule } from './tasks/task.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TaskModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const environment = configService.get<string>('NODE_ENV');
+
+  if (environment !== 'production') {
+    const config = new DocumentBuilder()
+      .setTitle('OSDRM API')
+      .setDescription('REST API OSDRM project 2026')
+      .setVersion('1.0')
+      .build();
+
+    const documentFactory = () => SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('documentation', app, documentFactory);
+  }
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
