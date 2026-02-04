46ed80c107d59b9a474b91cc04b3971a6502fedb
Ricardo <ricardoanjaraniaina8@gmail.com>
2026-02-04 15:43:44 +0300

    OSDRM-30 ajout de da-validation.controller et ses modules

--- src/purchaseValidation/purchase.module.ts
+++ src/purchaseValidation/purchase.module.ts
@@ -1,12 +1,46 @@
 import { Module } from '@nestjs/common';
-import { PurchaseService } from './services/purchase.service';
-import { PurchaseController } from './controllers/purchase.controller';
-import { PrismaModule } from 'prisma/prisma.module';
+import { PrismaService } from '../../prisma/prisma.service';
+import { PurchaseRepository } from 'src/repository/purchase/purchase.repository';
+import { UserRepository } from 'src/repository/user/user.repository';
+import { ValidatorRepository } from 'src/repository/purchase/validator.repository';
+import { ValidationWorkflowRepository } from 'src/repository/purchase/validation-workflow.repository';
+import { AuditLogRepository } from 'src/repository/audit/audit-log.repository';
+import { WorkflowConfigService } from './services/workflow-config.service';
+import { ValidationWorkflowService } from './services/validation-workflow.service';
+import { ValidationActionService } from './services/validation-action.service';
+import { PurchaseQueryService } from './services/purchase-query.service';
+import { AuthorizationService } from './services/authorization.service';
+import { DAValidationService } from './services/da-validation.service';
+import { DAValidationController } from './controllers/da-validation.controller';
 
 @Module({
-  imports: [PrismaModule],
-  controllers: [PurchaseController],
-  providers: [PurchaseService],
-  exports: [PurchaseService],
+  controllers: [DAValidationController],
+  providers: [
+    PrismaService,
+    PurchaseRepository,
+    UserRepository,
+    ValidationWorkflowRepository,
+    ValidatorRepository,
+    AuditLogRepository,
+    WorkflowConfigService,
+    ValidationWorkflowService,
+    ValidationActionService,
+    PurchaseQueryService,
+    AuthorizationService,
+    DAValidationService,
+  ],
+  exports: [
+    PurchaseRepository,
+    UserRepository,
+    ValidationWorkflowRepository,
+    ValidatorRepository,
+    AuditLogRepository,
+    WorkflowConfigService,
+    ValidationWorkflowService,
+    ValidationActionService,
+    PurchaseQueryService,
+    AuthorizationService,
+    DAValidationService,
+  ],
 })
 export class PurchaseModule {}
